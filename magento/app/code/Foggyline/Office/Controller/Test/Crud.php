0217dad9791f16b8d5d1d23f3fa00a077f9713d1
Manhtq <trinhquocmanh1990@gmail.com>
2022-03-02 14:38:43 +0700

    sp

--- magento/app/code/Foggyline/Office/Controller/Test/Crud.php
+++ magento/app/code/Foggyline/Office/Controller/Test/Crud.php
@@ -18,46 +18,48 @@ class Crud extends \Foggyline\Office\Controller\Test
     public function execute()
     {
         /* CRUD Code Here */
-        //Simple model, creating new entities, flavour #1
-        $department1 = $this->departmentFactory->create();
-        $department1->setName('Finance');
-        $department1->save();
-        //Simple model, creating new entities, flavour #2
-        $department2 = $this->departmentFactory->create();
-        $department2->setData('name', 'Research');
-        $department2->save();
-        //Simple model, creating new entities, flavour #3
-        $department3 = $this->departmentFactory->create();
-        $department3->setData(['name' => 'Support']);
-        $department3->save();
+        $create = false;
+        if ($create) {
+            //Simple model, creating new entities, flavour #1
+            $department1 = $this->departmentFactory->create();
+            $department1->setName('Finance');
+            $department1->save();
+            //Simple model, creating new entities, flavour #2
+            $department2 = $this->departmentFactory->create();
+            $department2->setData('name', 'Research');
+            $department2->save();
+            //Simple model, creating new entities, flavour #3
+            $department3 = $this->departmentFactory->create();
+            $department3->setData(['name' => 'Support']);
+            $department3->save();
 
-        //EAV model, creating new entities, flavour #1
-        $employee1 = $this->employeeFactory->create();
-        $employee1->setDepartment_id($department1->getId());
-        $employee1->setEmail('goran@mail.loc');
-        $employee1->setFirstName('Goran');
-        $employee1->setLastName('Gorvat');
-        $employee1->setServiceYears(3);
-        $employee1->setDob('1984-04-18');
-        $employee1->setSalary(3800.00);
-        $employee1->setVatNumber('GB123451234');
-        $employee1->setNote('Note #1');
-        $employee1->save();
-        //EAV model, creating new entities, flavour #2
-        $employee2 = $this->employeeFactory->create();
-        $employee2->setData('department_id', $department2->getId());
-        $employee2->setData('email', 'marko@mail.loc');
-        $employee2->setData('first_name', 'Marko');
-        $employee2->setData('last_name', 'Tunukovic');
-        $employee2->setData('service_years', 3);
-        $employee2->setData('dob', '1984-04-18');
-        $employee2->setData('salary', 3800.00);
-        $employee2->setData('vat_number', 'GB123451234');
-        $employee2->setData('note', 'Note #2');
-        $employee2->save();
-        //EAV model, creating new entities, flavour #3
-        $employee3 = $this->employeeFactory->create();
-        $employee3->setData([
+            //EAV model, creating new entities, flavour #1
+            $employee1 = $this->employeeFactory->create();
+            $employee1->setDepartment_id($department1->getId());
+            $employee1->setEmail('goran@mail.loc');
+            $employee1->setFirstName('Goran');
+            $employee1->setLastName('Gorvat');
+            $employee1->setServiceYears(3);
+            $employee1->setDob('1984-04-18');
+            $employee1->setSalary(3800.00);
+            $employee1->setVatNumber('GB123451234');
+            $employee1->setNote('Note #1');
+            $employee1->save();
+            //EAV model, creating new entities, flavour #2
+            $employee2 = $this->employeeFactory->create();
+            $employee2->setData('department_id', $department2->getId());
+            $employee2->setData('email', 'marko@mail.loc');
+            $employee2->setData('first_name', 'Marko');
+            $employee2->setData('last_name', 'Tunukovic');
+            $employee2->setData('service_years', 3);
+            $employee2->setData('dob', '1984-04-18');
+            $employee2->setData('salary', 3800.00);
+            $employee2->setData('vat_number', 'GB123451234');
+            $employee2->setData('note', 'Note #2');
+            $employee2->save();
+            //EAV model, creating new entities, flavour #3
+            $employee3 = $this->employeeFactory->create();
+            $employee3->setData([
             'department_id' => $department3->getId(),
             'email' => 'ivan@mail.loc',
             'first_name' => 'Ivan',
@@ -67,17 +69,20 @@ class Crud extends \Foggyline\Office\Controller\Test
             'salary' => 2400.00,
             'vat_number' => 'GB123454321',
             'note' => 'Note #3'
-        ]);
-        $employee3->save();
+            ]);
+            $employee3->save();
+        }
 
         //Simple model, reading existing entities
         $department = $this->departmentFactory->create();
-        $department->load(28);
-        \Zend_Debug::dump($department->toArray());
+        $department->load(2);
+        var_dump($department->toArray());
+        // \Zend\Debug::dump($department->toArray());
 
         //EAV model, reading existing entities
         $employee = $this->employeeFactory->create();
-        $employee->load(25);
-        \Zend_Debug::dump($employee->toArray());
+        $employee->load(10);
+        var_dump($employee->toArray());
+        // \Zend_Debug::dump($employee->toArray());
     }
 }
